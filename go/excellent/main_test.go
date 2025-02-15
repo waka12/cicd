1c2b924967f93a9dfe99ad7caab77e38eade20e5
rikuma <rikuma.wakabayashi@gmail.com>
2025-02-15 17:19:04 +0900

    fix test

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -1,10 +1,17 @@
 package main
 
+
+
 import "testing"
 
-func TestEvenOrOdd(t *testng.T) {
-  result := EvenOrOdd(10)
-  if result != "even" {
+
+
+func TestEvenOrOdd(t *testing.T) {
+ 
+ result := EvenOrOdd(10)
+ 
+ if result != "even" {
     t.Errorf("expected: even, actual: %s", result)
   }
+
 }
